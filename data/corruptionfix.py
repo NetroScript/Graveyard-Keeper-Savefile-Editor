NetroScript/Graveyard-Keeper-Savefile-Editor

commit 9c8be820dba71381a4e4fce4ed64813a39881400

0.1.3
closes #4
When we change the string while parsing, save the original buffer so the game can read it correctly
Also added info on how I built to the readme + updated the preview

data/corruptionfix.py
@@ -1,4 +1,3 @@
-
 # My personal save file from the beginning has an error in the string encoding of the Serializer which is fixed with the following code
 # You probably don't need it unless you have the same type of corruption in the savefile
 # If you get other errors and know what you are doing, feel free to add a fix for them here
@@ -6,19 +5,29 @@
 
 def prefix(buffer, stream, beginning, num):
 
+    length = len(buffer)
     # print(buffer)
     if str(buffer) == "[-48, -95, 2, 109, 11, 2, 31, 25, 77, 2, 11, 77, 11, 12, 4, 25]":
         buffer.append(stream.read("int8"))
+        return {"length": length, "buffer": buffer[:]}
     if str(buffer).endswith("-47]"):
         stream.file.seek(beginning + num+1)
+        return {"length": length, "buffer": buffer[:]}
 
     if str(buffer).startswith("[45, -47, -127,") or str(buffer).startswith("[-47, -127, 1") or str(buffer).startswith("[0, -47, -127,"):
         buffer.append(stream.read("int8"))
         stream.file.seek(beginning + num+1)
+        return {"length": length, "buffer": buffer[:]}
+
+    return False
 
 
 def postfix(string, stream, buffer):
     if string == "steep_yellow_blockage_0__destructio":
         buffer.append(stream.read("int8"))
         return "steep_yellow_blockage_0__destruction"
+    if string == "C4omfort of fait5":
+        return "Comfort of faith"
+    if "D" in string:
+        pass
     return string.replace("D", "C")

data/version
@@ -1,1 +1,1 @@
-0.1.2
+0.1.3

data/decode.py
@@ -162,7 +162,9 @@ class Decoder:
             # buffer.append(int.from_bytes(stream.read("char"), byteorder="little"))
             buffer.append(stream.read("int8"))
         l = stream.file.tell() - beginning
-        prefix(buffer, stream, beginning, l)
+
+        # We now add buffers and the length of the read to a special object, so we can save the orignal buffer when encoding again
+        specialread = prefix(buffer, stream, beginning, l)
 
         # print(num4)
         if encrypt:
@@ -180,6 +182,10 @@ class Decoder:
             out += str(char)
        
         out = postfix(out, stream, buffer)
+
+        if type(specialread) == dict:
+            out = {"string": out, "length": specialread["length"], "buffer": specialread["buffer"]}
+
         #print(out)
 
         return out

data/encode.py
@@ -77,8 +77,18 @@ class Encoder:
         elif curtype == Types.String_Empty:
             pass
         elif curtype == Types.String_Indexed:
-            #print(data)
-            stream.write("int32", serializer.index(data))
+
+            try:
+                indx = serializer.index(data)
+            except ValueError:
+                indx = 0
+                for i in range(len(serializer)):
+                    if type(serializer[i]) == dict:
+                        if data == serializer[i]["string"]:
+                            indx = i
+                            break
+
+            stream.write("int32", indx)
         elif curtype == Types.Int32_0:
             pass
         elif curtype == Types.Int32_1:
@@ -144,13 +154,18 @@ class Encoder:
         if string is None:
             stream.write("int32", -1)
             return
-        stream.write("int32", len(string))
-        for char in string:
-            n = ord(char)
-            if encrypt:
-                if n <= 255 and n != 0 and n != 109:
-                    n ^= 0x6D
-            stream.write("uint8", n)
+        if type(string) == dict:
+            stream.write("int32", string["length"])
+            for byte in string["buffer"]:
+                stream.write("int8", byte)
+        else:
+            stream.write("int32", len(string))
+            for char in string:
+                n = ord(char)
+                if encrypt:
+                    if n <= 255 and n != 0 and n != 109:
+                        n ^= 0x6D
+                stream.write("uint8", n)
     
     def insertserializer(self, stream, serializer):
         pos = stream.file.tell()

commit ae62e331eb96236a78f3bf5d69ca56a83b542369

0.1.5
closes #6 and closes #7

Fixed:

* A mistake on my side which partly breaks your save file, which was introduced in 9c8be820dba71381a4e4fce4ed64813a39881400 - It is only a small change but an own release because it breaks save files. If your save file was broken by this bug, leave me a message because the fix is rather simple (it is just adding a byte at a specific location).

data/corruptionfix.py
@@ -11,12 +11,10 @@ def prefix(buffer, stream, beginning, num):
         buffer.append(stream.read("int8"))
         return {"length": length, "buffer": buffer[:]}
     if str(buffer).endswith("-47]"):
-        stream.file.seek(beginning + num+1)
-        return {"length": length, "buffer": buffer[:]}
+        return {"length": length, "buffer": buffer[:] + [stream.read("int8")]}
 
     if str(buffer).startswith("[45, -47, -127,") or str(buffer).startswith("[-47, -127, 1") or str(buffer).startswith("[0, -47, -127,"):
         buffer.append(stream.read("int8"))
-        stream.file.seek(beginning + num+1)
         return {"length": length, "buffer": buffer[:]}
 
     return False

data/version
@@ -1,1 +1,1 @@
-0.1.4
+0.1.5